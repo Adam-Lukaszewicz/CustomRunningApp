40b577685815f24940f159042d4c61975fd90085
Adam Łukaszewicz <jakub.adam20@gmail.com>
2024-11-05 23:48:09 +0100

    basic communication with a dummy esp32

---
+++ custom_running_app/lib/services/bluetooth_service.dart
@@ -0,0 +1,122 @@
+import 'dart:io';
+
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+class BluetoothService {
+  static bool serviceEnabled = false;
+
+  static void initBluetooth() async {
+    // first, check if bluetooth is supported by your hardware
+// Note: The platform is initialized on the first call to any FlutterBluePlus method.
+    if (await FlutterBluePlus.isSupported == false) {
+      print("Bluetooth not supported by this device");
+      return;
+    }
+
+// handle bluetooth on & off
+// note: for iOS the initial state is typically BluetoothAdapterState.unknown
+// note: if you have permissions issues you will get stuck at BluetoothAdapterState.unauthorized
+    var subscription =
+        FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
+      print(state);
+      if (state == BluetoothAdapterState.on) {
+        // usually start scanning, connecting, etc
+      } else {
+        // show an error to the user, etc
+      }
+    });
+
+// turn on bluetooth ourself if we can
+// for iOS, the user controls bluetooth enable/disable
+    if (Platform.isAndroid) {
+      await FlutterBluePlus.turnOn();
+    }
+
+// cancel to prevent duplicate listeners
+    subscription.cancel();
+  }
+
+  static Stream<List<ScanResult>> getBluetoothDeviceList() {
+    var subscription = FlutterBluePlus.onScanResults.listen(
+      (results) {
+        if (results.isNotEmpty) {
+          ScanResult r = results.last; // the most recently found device
+          print(
+              '${r.device.remoteId}: "${r.advertisementData.advName}" found!');
+        } else {
+          print("no devices");
+        }
+      },
+      onError: (e) => print(e),
+    );
+
+// cleanup: cancel subscription when scanning stops
+    FlutterBluePlus.cancelWhenScanComplete(subscription);
+
+    FlutterBluePlus.adapterState.listen((states) async {
+      if (states == BluetoothAdapterState.on) {
+        await FlutterBluePlus.startScan(
+            // *or* any of the specified names
+            timeout: Duration(seconds: 30));
+      }
+    });
+
+// Start scanning w/ timeout
+// Optional: use `stopScan()` as an alternative to timeout
+    return FlutterBluePlus.onScanResults;
+  }
+
+  static void testDevice(BluetoothDevice device) async {
+    // listen for disconnection
+    var subscription =
+        device.connectionState.listen((BluetoothConnectionState state) async {
+      if (state == BluetoothConnectionState.disconnected) {
+        // 1. typically, start a periodic timer that tries to
+        //    reconnect, or just call connect() again right now
+        // 2. you must always re-discover services after disconnection!
+        print(
+            "${device.disconnectReason?.code} ${device.disconnectReason?.description}");
+      }
+    });
+
+// cleanup: cancel subscription when disconnected
+//   - [delayed] This option is only meant for `connectionState` subscriptions.
+//     When `true`, we cancel after a small delay. This ensures the `connectionState`
+//     listener receives the `disconnected` event.
+//   - [next] if true, the the stream will be canceled only on the *next* disconnection,
+//     not the current disconnection. This is useful if you setup your subscriptions
+//     before you connect.
+    device.cancelWhenDisconnected(subscription, delayed: true, next: true);
+
+// Connect to the device
+    await device.connect();
+    // Note: You must call discoverServices after every re-connection!
+    var services = await device.discoverServices();
+    services.forEach((service) {
+      if (service.serviceUuid.toString() ==
+          "cd9cfc21-0ecc-42e5-bf22-48aa715ca112") {
+        service.characteristics.forEach((characteristic) async {
+          if (characteristic.uuid.toString() ==
+              "66E5FFCE-AA96-4DC9-90C3-C62BBCCD29AC".toLowerCase()) {
+            String toWrite = "TEST";
+            characteristic.write(toWrite.codeUnits);
+          }
+          if (characteristic.uuid.toString() ==
+              "142F29DD-B1F0-4FA8-8E55-5A2D5F3E2471".toLowerCase()) {
+            if (characteristic.properties.read) {
+              List<int> value = await characteristic.read();
+              print(String.fromCharCodes(value));
+            }
+          }
+        });
+      }
+      // do something with service
+    });
+
+// Disconnect from device
+    await device.disconnect();
+
+// cancel to prevent duplicate listeners
+    subscription.cancel();
+  }
+}
