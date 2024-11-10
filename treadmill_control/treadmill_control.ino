d4f4ceb66e13f0a7010f35af66e1be510544c79a
kobie <kobielus63@gmail.com>
2024-11-10 17:48:17 +0100

    change threadmill incline

--- treadmill_control/treadmill_control.ino
+++ treadmill_control/treadmill_control.ino
@@ -4,6 +4,9 @@ int potentiometerPin = A0; // Pin do odczytu potencjometru
 int upPin=D1;   //Pin do podnoszenia bieżni
 int downPin=D3; //Pin do obniżania bieżni
 int motorPwmPin = D2; // Pin, na którym będzie generowany PWM
+int speedReadPin = D4; // Pin D4 do przerwania
+int ledPin = D0;  // Wbudowana dioda LED na pinie D0
+int givenSpeed=0;
 
 
 //zmienne pomocnicze
@@ -13,8 +16,75 @@ int targetValue = 255 * 0.3; // Docelowa wartość PWM (30% z 255)
 int potentiometerValue = 0; // Zmienna do przechowywania wartości potencjometru
 
 
+enum state{
+  rising=0,
+  lowering=1,
+  standby=2,
+};
+
+struct incline{
+  int givenValue;
+  state givenState;
+  incline(int gv = 0, state rising = state::standby) : givenValue(gv), givenState(rising) {}
+};
+
+incline givenIncline(0,state::standby);
+int currentIncline=0;
+
+#define potentiometer_min_value 99
+#define potentiometer_max_value 1100
+#define treadmill_min_incline 0.0
+#define treadmill_max_incline 10.0
+void get_treadmill_incline(double incline){
+  //funkcja zamienia kąt na wartość potencjometru do osiągnięcia
+  givenIncline.givenValue = int( (incline - treadmill_min_incline) * (potentiometer_max_value - potentiometer_min_value) / (treadmill_max_incline - treadmill_min_incline) + potentiometer_min_value);
+  if (givenIncline.givenValue>currentIncline){
+    givenIncline.givenState  = state::rising;
+  }
+  else if(givenIncline.givenValue<currentIncline ){
+     givenIncline.givenState= state::lowering;
+  }
+  else {
+    givenIncline.givenState= state::standby;
+  }
+
+}
+
+void change_incline(){
+if(givenIncline.givenState==state::lowering){
+
+  if(givenIncline.givenValue>analogRead(potentiometerPin)){
+  digitalWrite(downPin, LOW);
+  digitalWrite(upPin, LOW);}
+  else{
+  digitalWrite(downPin, HIGH);
+  digitalWrite(upPin, LOW);}
+}
+else if(givenIncline.givenState==state::rising){
+
+  if(givenIncline.givenValue<analogRead(potentiometerPin)){
+  digitalWrite(downPin, LOW);
+  digitalWrite(upPin, LOW);}
+  else{
+  digitalWrite(downPin, LOW);
+  digitalWrite(upPin, HIGH);}
+}else{
+  digitalWrite(downPin, LOW);
+  digitalWrite(upPin, LOW);}
+}
+
+// Funkcja obsługująca przerwanie
+ICACHE_RAM_ATTR void handleInterrupt() {
+  digitalWrite(ledPin, HIGH);  // Włącz LED
+  delay(100);                   // Czekaj 100 ms
+  digitalWrite(ledPin, LOW);   // Wyłącz LED
+}
 
 void setup() {
+
+pinMode(ledPin, OUTPUT);    // Ustawienie pinu D0 jako wyjście (LED)
+
+attachInterrupt(digitalPinToInterrupt(speedReadPin), handleInterrupt, FALLING);
 
   analogWriteFreq(10000);  
   pinMode(motorPwmPin, OUTPUT);
@@ -43,16 +113,6 @@ void setup() {
 
 void loop() {
 
-potentiometerValue = analogRead(potentiometerPin); 
-if (potentiometerValue < 110) {
-digitalWrite(downPin, LOW);
-delay(5000);
-digitalWrite(upPin, HIGH);
-}
-else if (potentiometerValue > 950) {
-digitalWrite(upPin, LOW);
-delay(5000);
-digitalWrite(downPin, HIGH);
-}
+change_incline();
 
 }
