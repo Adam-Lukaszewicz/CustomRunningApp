7ee175f02dbee30f771b048173da5417b9fd026a
kobie <kobielus63@gmail.com>
2024-11-10 19:34:50 +0100

    comeads over serial port and inlcline control

--- treadmill_control/treadmill_control.ino
+++ treadmill_control/treadmill_control.ino
@@ -6,15 +6,19 @@ int downPin=D3; //Pin do obniżania bieżni
 int motorPwmPin = D2; // Pin, na którym będzie generowany PWM
 int speedReadPin = D4; // Pin D4 do przerwania
 int ledPin = D0;  // Wbudowana dioda LED na pinie D0
-int givenSpeed=0;
+String incomingCommand = "";
 
 
 //zmienne pomocnicze
 unsigned long startTime; // Czas rozpoczęcia
 int pwmValue = 0; // Wartość PWM, która będzie się zmieniać
 int targetValue = 255 * 0.3; // Docelowa wartość PWM (30% z 255)
-int potentiometerValue = 0; // Zmienna do przechowywania wartości potencjometru
 
+
+#define potentiometer_min_value 1100
+#define potentiometer_max_value 110
+#define treadmill_min_incline 0.0
+#define treadmill_max_incline 10.0
 
 enum state{
   rising=0,
@@ -28,20 +32,25 @@ struct incline{
   incline(int gv = 0, state rising = state::standby) : givenValue(gv), givenState(rising) {}
 };
 
-incline givenIncline(0,state::standby);
-int currentIncline=0;
+incline givenIncline(potentiometer_min_value,state::standby);
 
-#define potentiometer_min_value 99
-#define potentiometer_max_value 1100
-#define treadmill_min_incline 0.0
-#define treadmill_max_incline 10.0
-void get_treadmill_incline(double incline){
-  //funkcja zamienia kąt na wartość potencjometru do osiągnięcia
+void set_potentiometr_value_for_incline(double incline){
+  if(incline<0||incline>10){
+    //wartość nachylenia z poza zakresu
+    return;
+  }
+  //funkcja zamienia kąt którą potencjometr ma osiągnąć
+
+if(incline==0){
+  givenIncline.givenValue=potentiometer_min_value;
+}
+else{
   givenIncline.givenValue = int( (incline - treadmill_min_incline) * (potentiometer_max_value - potentiometer_min_value) / (treadmill_max_incline - treadmill_min_incline) + potentiometer_min_value);
-  if (givenIncline.givenValue>currentIncline){
+}
+  if (givenIncline.givenValue>analogRead(potentiometerPin)){
     givenIncline.givenState  = state::rising;
   }
-  else if(givenIncline.givenValue<currentIncline ){
+  else if(givenIncline.givenValue<analogRead(potentiometerPin) ){
      givenIncline.givenState= state::lowering;
   }
   else {
@@ -55,7 +64,9 @@ if(givenIncline.givenState==state::lowering){
 
   if(givenIncline.givenValue>analogRead(potentiometerPin)){
   digitalWrite(downPin, LOW);
-  digitalWrite(upPin, LOW);}
+  digitalWrite(upPin, LOW);
+  givenIncline.givenState= state::standby;
+  }
   else{
   digitalWrite(downPin, HIGH);
   digitalWrite(upPin, LOW);}
@@ -64,24 +75,28 @@ else if(givenIncline.givenState==state::rising){
 
   if(givenIncline.givenValue<analogRead(potentiometerPin)){
   digitalWrite(downPin, LOW);
-  digitalWrite(upPin, LOW);}
+  digitalWrite(upPin, LOW);
+  givenIncline.givenState= state::standby;
+  }
   else{
   digitalWrite(downPin, LOW);
   digitalWrite(upPin, HIGH);}
-}else{
+}else{//givenIncline.givenState==state::standby
   digitalWrite(downPin, LOW);
   digitalWrite(upPin, LOW);}
 }
 
 // Funkcja obsługująca przerwanie
 ICACHE_RAM_ATTR void handleInterrupt() {
-  digitalWrite(ledPin, HIGH);  // Włącz LED
-  delay(100);                   // Czekaj 100 ms
+  digitalWrite(ledPin, HIGH);  // Włącz LED               // Czekaj 100 ms
   digitalWrite(ledPin, LOW);   // Wyłącz LED
 }
 
 void setup() {
-
+Serial.begin(9600);
+  while (!Serial) {
+    ; // czekaj na połączenie z portem szeregowym
+  }
 pinMode(ledPin, OUTPUT);    // Ustawienie pinu D0 jako wyjście (LED)
 
 attachInterrupt(digitalPinToInterrupt(speedReadPin), handleInterrupt, FALLING);
@@ -95,7 +110,7 @@ attachInterrupt(digitalPinToInterrupt(speedReadPin), handleInterrupt, FALLING);
 
 
   startTime = millis();
-
+set_potentiometr_value_for_incline(0);
 //stopniowo rozpędź silnik
   while (true) {
     unsigned long elapsedTime = millis() - startTime;
@@ -112,7 +127,44 @@ attachInterrupt(digitalPinToInterrupt(speedReadPin), handleInterrupt, FALLING);
 }
 
 void loop() {
+recive_serial_command();
 
 change_incline();
 
 }
+
+void recive_serial_command(){
+  if (Serial.available() > 0) {
+    char receivedChar = Serial.read();  // Read a single character
+    if (receivedChar == '\n') {
+      // Newline received, process the command
+      Serial.print("Odebrano komendę: ");
+      Serial.println(incomingCommand);
+            int spaceIndex = incomingCommand.indexOf(' ');
+      if (spaceIndex > 0) {
+        // Extract command name and parameter from the incoming command
+        String commandName = incomingCommand.substring(0, spaceIndex);
+        String parameterString = incomingCommand.substring(spaceIndex + 1);
+        double parameterValue = parameterString.toDouble();
+        Serial.print("Command: ");
+        Serial.println(commandName);
+        Serial.print("Parameter: ");
+        Serial.println(parameterValue);
+        handle_serial_command(commandName, parameterValue);
+      }
+incomingCommand = "";
+    }
+    else {
+      incomingCommand += receivedChar;
+    }
+
+  }
+}
+
+void handle_serial_command(String name, double param) {
+    if (name == "incline") {
+        set_potentiometr_value_for_incline(param);
+    }
+
+}
+
